3cdaf6d3f51d3d76e8c28952d406bc15ba204443
banku27 <pankajparihar123321@gmail.com>
2023-05-21 00:32:42 +0530

    Onbaording sreens

--- lib/views/ui/onboarding/widgets/page_three.dart
+++ lib/views/ui/onboarding/widgets/page_three.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-
-import '../../../../constants/app_constants.dart';
+import 'package:job_finder/views/common/custom_outline_btn.dart';
+import 'package:job_finder/views/common/exports.dart';
+import 'package:job_finder/views/common/height_spacer.dart';
 
 class PageThree extends StatelessWidget {
   const PageThree({super.key});
@@ -8,11 +9,54 @@ class PageThree extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: width,
-        height: hieght,
-        color: Color(kDarkBlue.value),
-      ),
-    );
+        body: Container(
+      width: width,
+      height: hieght,
+      color: Color(kLightBlue.value),
+      child: Column(children: [
+        const HeightSpacer(size: 20),
+        Image.asset('assets/images/page3.png'),
+        const HeightSpacer(size: 40),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+          child: ReusableText(
+            text: 'Welcome To Job Finder',
+            style: appstyle(30, Color(kLight.value), FontWeight.w600),
+          ),
+        ),
+        const HeightSpacer(size: 15),
+        Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+          CustomOutlineBtn(
+            onTap: () {},
+            text: 'Login',
+            width: width * 0.4,
+            height: hieght * 0.06,
+            color: Color(kLight.value),
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Container(
+              width: width * 0.4,
+              height: hieght * 0.06,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Color(kLight.value),
+              ),
+              child: Center(
+                child: ReusableText(
+                  text: 'Sign Up',
+                  style: appstyle(16, Color(kLightBlue.value), FontWeight.w600),
+                ),
+              ),
+            ),
+          )
+        ]),
+        const HeightSpacer(size: 30),
+        ReusableText(
+          text: 'Continue as guest',
+          style: appstyle(16, Color(kLight.value), FontWeight.w400),
+        ),
+      ]),
+    ));
   }
 }
--- lib/views/ui/onboarding/widgets/page_two.dart
+++ lib/views/ui/onboarding/widgets/page_two.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:job_finder/views/common/app_style.dart';
+import 'package:job_finder/views/common/height_spacer.dart';
 
 import '../../../../constants/app_constants.dart';
 
@@ -11,7 +14,32 @@ class PageTwo extends StatelessWidget {
         body: Container(
       width: width,
       height: hieght,
-      color: Color(kDarkPurple.value),
+      color: Color(kDarkBlue.value),
+      child: Column(
+        children: [
+          const HeightSpacer(size: 65),
+          Padding(
+            padding: EdgeInsets.all(8.0.h),
+            child: Image.asset('assets/images/page2.png'),
+          ),
+          const HeightSpacer(size: 20),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Stable Yourself \n With Your Ability',
+                textAlign: TextAlign.center,
+                style: appstyle(30, Color(kLight.value), FontWeight.w500),
+              ),
+              const HeightSpacer(size: 20),
+              // Text(
+              //   'data',
+              //   style: appstyle(14, Color(kLightGrey.value), FontWeight.normal),
+              // )
+            ],
+          )
+        ],
+      ),
     ));
   }
 }
