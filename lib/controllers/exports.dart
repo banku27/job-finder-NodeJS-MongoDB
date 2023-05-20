0ea2f379056bf111033514b5e3e1fdcf47cb4749
banku27 <pankajparihar123321@gmail.com>
2023-05-20 13:43:44 +0530

    Project Setup

---
+++ lib/controllers/exports.dart
@@ -0,0 +1,8 @@
+export 'package:job_finder/controllers/bookmark_provider.dart';
+export 'package:job_finder/controllers/image_provider.dart';
+export 'package:job_finder/controllers/jobs_provider.dart';
+export 'package:job_finder/controllers/login_provider.dart';
+export 'package:job_finder/controllers/onboarding_provider.dart';
+export 'package:job_finder/controllers/profile_provider.dart';
+export 'package:job_finder/controllers/signup_provider.dart';
+export 'package:job_finder/controllers/zoom_provider.dart';
---
+++ lib/views/common/exports.dart
@@ -0,0 +1,3 @@
+export 'package:job_finder/constants/app_constants.dart';
+export 'package:job_finder/views/common/app_style.dart';
+export 'package:job_finder/views/common/reusable_text.dart';
