d4a1a06dbabcee36e29ba6d69bd4ef3d5a807712
banku27 <pankajparihar123321@gmail.com>
2023-06-08 11:23:40 +0530

    drawer screen and drawer item

---
+++ lib/views/ui/drawer/drawer_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
+import 'package:job_finder/constants/app_constants.dart';
+import 'package:job_finder/controllers/exports.dart';
+import 'package:job_finder/views/common/exports.dart';
+import 'package:job_finder/views/common/width_spacer.dart';
+import 'package:provider/provider.dart';
+
+class DrawerScreen extends StatefulWidget {
+  final ValueSetter indexSetter;
+  const DrawerScreen({super.key, required this.indexSetter});
+
+  @override
+  State<DrawerScreen> createState() => _DrawerScreenState();
+}
+
+class _DrawerScreenState extends State<DrawerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ZoomNotifier>(
+      builder: (context, zoomNotifier, child) {
+        return GestureDetector(
+          onDoubleTap: () {
+            ZoomDrawer.of(context)!.toggle();
+          },
+          child: Scaffold(
+            backgroundColor: Color(kLightBlue.value),
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                drawerItem(
+                  AntDesign.home,
+                  'Home',
+                  0,
+                  zoomNotifier.currentIndex == 0
+                      ? Color(kLight.value)
+                      : Color(kLightGrey.value),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget drawerItem(IconData icon, String text, int index, Color color) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        margin: EdgeInsets.only(left: 20.w, bottom: 20.h),
+        child: Row(
+          children: [
+            Icon(
+              icon,
+              color: color,
+            ),
+            const WidthSpacer(
+              size: 12,
+            ),
+            ReusableText(
+                text: text,
+                style: appstyle(
+                  12,
+                  color,
+                  FontWeight.w500,
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
